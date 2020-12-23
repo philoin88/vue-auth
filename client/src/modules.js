0e218a7e0373bf528300f0f9c561194d3ead2988
Robin <philoin88@gmail.com>
2020-12-23 14:44:52 +0900

    util.js 모듈 추가

--- client/src/modules.js
+++ client/src/modules.js
@@ -20,6 +20,30 @@ const common = {
  },
 };
 
+
+
+// 커스텀 util에 관한 기능 정의
+const util = {
+  // Vue 내부에서 사용하는 Vue 객체 (.vue 파일에서 this)
+  vm: {},
+  // util 초기화 함수 [Vue 객체를 vm 변수에 세팅]
+  init(vm) {
+    this.vm = vm;
+  },
+  // Null, Undefined 체크 함수
+  isNull(target) {
+    return target == null || typeof target === 'undefined';
+  },
+  // object null 체크함수
+  isEmpty(object) {
+    return Object.keys(object).length === 0;
+  },
+  // Null, Undefined, Empty String 체크 함수
+  isEmptyString(target) {
+    return this.isNull(target) || target === '';
+  }
+}
+
 // Module을 Vue 전역 Property로 등록
 let EventBus = new Vue();
 Object.defineProperties(Vue.prototype, {
@@ -32,6 +56,12 @@ Object.defineProperties(Vue.prototype, {
     get() {
       common.init(this);
       return common;
+    }
+  },
+  util: {
+    get() {
+      util.init(this);
+      return util;
     }
   }
 });
